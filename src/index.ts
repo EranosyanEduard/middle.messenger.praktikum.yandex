cde002a98ac92d6c2ab52d2746d5feb5b7f34214
EranosyanEduard <EranosyanEduard@yandex.ru>
2022-05-24 08:52:54 +0300

    fix: исправить навигацию на сервере

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,13 @@ import path from "path"
 
 const app = express()
 const dirname = path.resolve()
+const pathToDirWithEntryPoint = path.join(dirname, "dist")
 
-app.use(express.static(path.join(dirname, "dist")))
+app.use(express.static(pathToDirWithEntryPoint))
+
+app.all("*", (_req, res) => {
+    res.sendFile(`${pathToDirWithEntryPoint}/index.html`)
+})
 
 app.listen(3000, () => {
     console.log("Сервер запущен")
