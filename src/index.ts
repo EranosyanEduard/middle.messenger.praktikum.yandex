60cffbb4dd294fadb44d35165afbcd04a7082bc8
EranosyanEduard <EranosyanEduard@yandex.ru>
2022-07-19 11:00:16 +0300

    feat: добавить поддержку heroku

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import "dotenv/config"
 import express from "express"
 import path from "path"
 
-const {EXPRESS_SERVER_PORT = 3000} = process.env
+const {PORT = 3000} = process.env
 
 const app = express()
 const dirname = path.resolve()
@@ -14,6 +14,6 @@ app.all("*", (_, res) => {
     res.sendFile(`${pathToDirWithEntryPoint}/index.html`)
 })
 
-app.listen(EXPRESS_SERVER_PORT, () => {
-    console.log(`Server listening on port ${EXPRESS_SERVER_PORT}`)
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`)
 })
